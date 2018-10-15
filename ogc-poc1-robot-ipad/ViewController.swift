69c3814ffde7bc879f80c2f8979f034683d791c5
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2018-10-15 14:08:27 +0900

    add Settings.Bundle to store the settings to connect MQTT Broker

--- ogc-poc1-robot-ipad/ViewController.swift
+++ ogc-poc1-robot-ipad/ViewController.swift
@@ -10,10 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var host: String {
+        return UserDefaults.standard.string(forKey: "host") ?? ""
+    }
+    var port: Int {
+        return UserDefaults.standard.integer(forKey: "port")
+    }
+    var username: String {
+        return UserDefaults.standard.string(forKey: "username") ?? ""
+    }
+    var password: String {
+        return UserDefaults.standard.string(forKey: "password") ?? ""
+    }
+    var basetopic: String {
+        return UserDefaults.standard.string(forKey: "basetopic") ?? ""
+    }
+    
     @IBOutlet weak var label: UILabel!
+    
+    func registerSettingsBundle(){
+        let appDefaults = [String:AnyObject]()
+        UserDefaults.standard.register(defaults: appDefaults)
+        UserDefaults.standard.synchronize()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        registerSettingsBundle()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
