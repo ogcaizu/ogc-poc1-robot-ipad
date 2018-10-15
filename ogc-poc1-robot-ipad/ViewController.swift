b25bfff7569b69c6b609c3944855068282752f33
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2018-10-15 12:53:06 +0900

    Initial Commit

---
+++ ogc-poc1-robot-ipad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ogc-poc1-robot-ipad
+//
+//  Created by Nobuyuki Matsui on 2018/10/15.
+//  Copyright © 2018年 Nobuyuki Matsui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
