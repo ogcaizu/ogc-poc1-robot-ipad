ed8038f60d76a5ed9f0d2c240702fd224a4078b4
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2018-10-15 18:04:42 +0900

    connect MQTT Broker and subscribe fiware's command

--- ogc-poc1-robot-ipad/AppDelegate.swift
+++ ogc-poc1-robot-ipad/AppDelegate.swift
@@ -12,6 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var mainView: ViewController?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
@@ -27,10 +28,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        print("applicationDidEnterBackground")
+        mainView?.mqtt?.disconnect()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        print("applicationWillEnterForeground")
+        mainView?.mqtt?.connect()
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
--- ogc-poc1-robot-ipad/ViewController.swift
+++ ogc-poc1-robot-ipad/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import CocoaMQTT
+
+class Messages {
+    static let connectionError = "接続失敗"
+    static var waiting: String {
+        return "誘導ロボット\n\(UserDefaults.standard.string(forKey: "location") ?? "1F")"
+    }
+    static let guiding = "ご案内中"
+    static let suspending = "目的地に\n到着しました"
+    static let returning = "帰還中"
+}
+
+enum Command: String {
+    case state
+}
+
+enum State: String {
+    case Waiting
+    case Guiding
+    case Suspending
+    case Returning
+}
 
 class ViewController: UIViewController {
+    var mqtt: CocoaMQTT?
+    let messages = Messages()
+    
+    let re = try! Regex("([\\w_-]+)@([\\w-_]+)\\|([\\w-_]+)")
 
     var host: String {
         return UserDefaults.standard.string(forKey: "host") ?? ""
     }
-    var port: Int {
-        return UserDefaults.standard.integer(forKey: "port")
+    var port: UInt16 {
+        return UInt16(UserDefaults.standard.integer(forKey: "port"))
+    }
+    var enableSSL: Bool {
+        return UserDefaults.standard.bool(forKey: "enableSSL")
     }
     var username: String {
         return UserDefaults.standard.string(forKey: "username") ?? ""
@@ -22,21 +51,47 @@ class ViewController: UIViewController {
     var password: String {
         return UserDefaults.standard.string(forKey: "password") ?? ""
     }
-    var basetopic: String {
-        return UserDefaults.standard.string(forKey: "basetopic") ?? ""
+    var cmdTopic: String {
+        return "\(UserDefaults.standard.string(forKey: "basetopic") ?? "")/cmd"
+    }
+    var cmdExeTopic: String {
+        return "\(UserDefaults.standard.string(forKey: "basetopic") ?? "")/cmdexe"
     }
     
     @IBOutlet weak var label: UILabel!
     
-    func registerSettingsBundle(){
+    func registerSettingsBundle() {
         let appDefaults = [String:AnyObject]()
         UserDefaults.standard.register(defaults: appDefaults)
         UserDefaults.standard.synchronize()
     }
     
+    func setupMQTT() {
+        print("setup MQTT, host=\(host), port=\(port), enableSSL=\(enableSSL), username=\(username), password=\(password)")
+        let clientID = "CocoaMQTT-ogc-poc1-robot-ipad-" + String(ProcessInfo().processIdentifier)
+        mqtt = CocoaMQTT(clientID: clientID, host: host, port: port)
+        mqtt!.username = username
+        mqtt!.password = password
+        mqtt!.keepAlive = 60
+        if enableSSL {
+            mqtt!.enableSSL = true
+            mqtt!.allowUntrustCACertificate = true
+        } else {
+            mqtt!.enableSSL = false
+        }
+        mqtt!.delegate = self
+        let connected = mqtt!.connect()
+        if !connected {
+            label.text = Messages.connectionError
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let appDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.mainView = self
         registerSettingsBundle()
+        setupMQTT()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -44,7 +99,100 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ViewController: CocoaMQTTDelegate {
+    func execPayload(_ payload: String) -> (String, Bool) {
+        var result = "invalid message format, message=\(payload)"
+        var publishable = false
+        if let match = re.firstMatch(payload) {
+            if let deviceID = match.groups[0], let cmd = match.groups[1], let state = match.groups[2] {
+                if let c = Command(rawValue: cmd) {
+                    switch c {
+                    case .state:
+                        if let s = State(rawValue: state) {
+                            switch s {
+                            case .Waiting:
+                                label.text = Messages.waiting
+                                result = "executed Waiting"
+                            case .Guiding:
+                                label.text = Messages.guiding
+                                result = "executed Guiding"
+                            case .Suspending:
+                                label.text = Messages.suspending
+                                result = "executed Suspending"
+                            case .Returning:
+                                label.text = Messages.returning
+                                result = "executed Returning"
+                            }
+                        } else {
+                            result = "unknown state, state=\(state), message=\(payload)"
+                        }
+                    }
+                } else {
+                    result = "unknown cmd, cmd=\(cmd), message=\(payload)"
+                }
+                publishable = true
+                result = "\(deviceID)@\(cmd)|\(result)"
+            }
+        }
+        return (result, publishable)
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didReceive trust: SecTrust, completionHandler: @escaping (Bool) -> Void) {
+        print("mqtt didReceive trust\(trust)")
+        completionHandler(true)
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didConnectAck ack: CocoaMQTTConnAck) {
+        print("mqtt didConnectAck ack=\(ack.rawValue)")
+        if ack == .accept {
+            label.text = Messages.waiting
+            mqtt.subscribe(cmdTopic, qos: CocoaMQTTQOS.qos0)
+        } else {
+            label.text = Messages.connectionError
+        }
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didReceiveMessage message: CocoaMQTTMessage, id: UInt16 ) {
+        print("mqtt didReceiveMessage message=\(message.string ?? "empty message") id=\(id)")
+        if let payload = message.string {
+            let (result, publishable) = execPayload(payload)
+            print("execPayload result=\(result)")
+            if publishable {
+                mqtt.publish(cmdExeTopic, withString: result, qos: CocoaMQTTQOS.qos0)
+            }
+        }
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didPublishMessage message: CocoaMQTTMessage, id: UInt16) {
+        print("mqtt didPublishMessage message=\(message.string!) id=\(id)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didPublishAck id: UInt16) {
+        print("mqtt didPublishAck id=\(id)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didSubscribeTopic topic: String) {
+        print("mqtt didSubscribeTopic topic=\(topic)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didUnsubscribeTopic topic: String) {
+        print("mqtt didUnsubscribeTopic topic=\(topic)")
+    }
+    
+    func mqttDidPing(_ mqtt: CocoaMQTT) {
+        print("mqttDidPing")
+    }
+    
+    func mqttDidReceivePong(_ mqtt: CocoaMQTT) {
+        print("mqttDidReceivePong")
+    }
+    
+    func mqttDidDisconnect(_ mqtt: CocoaMQTT, withError err: Error?) {
+        print("mqttDidDisconnect withError err=\(String(describing: err))")
+        if err != nil {
+            label.text = Messages.connectionError
+        }
+    }
+}
